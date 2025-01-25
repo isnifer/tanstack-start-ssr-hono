768725c708c64b921eb7008450c805796d53a14d
Anton Kuznetsov <isnifer@gmail.com>
2025-01-25 22:43:55 +0400

    Avoid usage of Bun

--- app/api.ts
+++ app/api.ts
@@ -1,6 +1,7 @@
 import { createStartAPIHandler } from '@tanstack/start/api'
+import * as process from 'node:process'
 import server from './server'
 
 export default createStartAPIHandler(({ request }) =>
-  server.fetch(request, { NODE_ENV: Bun.env.NODE_ENV })
+  server.fetch(request, { NODE_ENV: process.env.NODE_ENV })
 )
