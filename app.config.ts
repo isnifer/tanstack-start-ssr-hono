9f11187c3d2402f587a489f534ad563e1fae5079
Anton Kuznetsov <isnifer@gmail.com>
2025-01-26 06:45:47 +0400

    Try to build on Render

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,12 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from '@tanstack/start/config'
+import * as process from 'node:process'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
+const preset = process.env.PRODUCTION_PRESET || 'bun'
+
 export default defineConfig({
-  server: {
-    // preset: 'bun',
-    preset: 'vercel',
-  },
+  server: { preset },
   vite: {
     plugins: [
       tailwindcss(),
