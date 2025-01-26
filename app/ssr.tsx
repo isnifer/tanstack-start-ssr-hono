5a5c7b715d45cb1241b4152c40c8277a6cf9df9e
Anton Kuznetsov <isnifer@gmail.com>
2025-01-26 11:59:57 +0400

    Log production preset

--- app/ssr.tsx
+++ app/ssr.tsx
@@ -12,7 +12,7 @@ const preset = process.env.PRODUCTION_PRESET || 'bun'
 const handler: typeof defaultStreamHandler = async ({ request, router, responseHeaders }) => {
   const { pathname } = new URL(request.url)
 
-  console.log('!!!! PATHNAME !!!!', pathname)
+  console.log('!!!! PATHNAME !!!!', pathname, preset)
 
   if (pathname === '/bun-stream-error' && preset === 'bun') {
     return defaultStreamHandler({ request, router, responseHeaders })
